68cec6f664daa99624951df8f46245b7a1603752
jason0905 <jliu@davidsonacademy.unr.edu>
2015-03-06 16:58:57 -0800

    Finished dualpal

---
+++ dualpal.cc
@@ -0,0 +1,58 @@
+/*
+ID: jason091
+LANG: C++
+TASK: dualpal
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string changeBase(int n, int b) {
+    string x;
+
+    for(int i = 0; n > 0; i++) {
+        if((n % b) < 10)
+            x += (n % b) + 48;
+        else
+            x += ((n % b) + 55);
+
+        n /= b;
+    }
+
+    return x;
+}
+
+bool hasTen(int x) {
+    string y;
+    for(int i = 2, j = 0; i < 11; i++) {
+        y = changeBase(x, i);
+        for(int k = 0, l = y.size() - 1; k <= l; k++, l--) {
+            if(y[k] != y[l]) break;
+            else if(k + 1 >= l) {
+                j++;
+                break;
+            }
+        }
+        if(j == 2) return true;
+    }
+    return false;
+}
+
+int main() {
+    ifstream in ("dualpal.in");
+    ofstream out ("dualpal.out");
+
+    int N, S, x = 0;
+
+    in >> N >> S;
+
+    for(int i = S + 1; x < N; i++) {
+        if(hasTen(i)) {
+            x++;
+            out << i << endl;
+        }
+    }
+}
