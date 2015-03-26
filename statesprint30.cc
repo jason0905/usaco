5a2ab63ac1fdb5b9404e09ab0813edbb456c0e6b
jason0905 <jliu@davidsonacademy.unr.edu>
2015-03-25 20:08:43 -0700

    Finished statesprint30

--- statesprint30.cc
+++ statesprint30.cc
@@ -1,10 +1,26 @@
 #include <vector>
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
-void squarenum(int N, int count, vector<int> square, int layer) {
-    if(layer == N) count++;
+void squarenum(int & N, int & count, vector<int> & square, int layer) {
+    if(layer == N * N) {
+        count++;
+        return;
+    }
+    if(square[0] != N) {
+        square[0]++;
+        squarenum(N, count, square, layer + 1);
+        square[0]--;
+    }
+    for(int i = 1; i < N; i++) {
+        if(square[i] < square[i - 1]) {
+            square[i]++;
+            squarenum(N, count, square, layer + 1);
+            square[i]--;
+        }
+    }
 }
 
 int main() {
@@ -13,9 +29,10 @@ int main() {
 
     int N, count = 0;
     vector<int> square;
-    for(int i = 0; i < N; i++) {
+    in >> N;
+    for(int i = 0; i < N; i++)
         square.push_back(0);
-    }
+
     squarenum(N, count, square, 1);
 
     out << count << endl;
